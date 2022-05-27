cad6c9455dd5ccddba72872112044e94f6c5c428
konarsada <sadabratak@gmail.com>
2022-05-27 22:47:15 +0530

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react"
+import {Switch, Route} from "react-router-dom"
+
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
@@ -7,7 +9,16 @@ function App() {
     return (
         <div>
             <Header />
-            <h1>Home Page</h1>
+
+            <Switch>
+                <Route exact path="/">
+                    <Photos />
+                </Route>
+
+                <Route path="/cart">
+                    <Cart />
+                </Route>
+            </Switch>
         </div>
     )
 }
---
+++ src/components/Image.js
@@ -0,0 +1,48 @@
+import React, { useState, useContext } from "react"
+import PropTypes from "prop-types"
+
+import {Context} from "../Context"
+
+function Image({ className, img }) {
+    const [hovered, setHovered] = useState(false)
+
+    const {toggleFavorite} = useContext(Context)
+
+    function heartIcon() {
+        if(img.isFavorite) {
+            return <i className="ri-heart-fill favorite"
+                onClick={() => toggleFavorite(img.id)}>
+            </i>
+        }
+        else if(hovered) {
+            return <i className="ri-heart-line favorite"
+                onClick={() => toggleFavorite(img.id)}>
+            </i>
+        }
+    }
+    
+    const cartIcon = hovered &&
+        <i className="ri-add-circle-line cart"></i>
+
+    return (
+        <div className={`${className} image-container`}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
+            <img src={img.url} alt="products" className="image-grid" />
+            {heartIcon()}
+            {cartIcon}
+        </div>
+    )
+}
+
+Image.propTypes = {
+    className: PropTypes.string,
+    img: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool
+    })
+}
+
+export default Image
